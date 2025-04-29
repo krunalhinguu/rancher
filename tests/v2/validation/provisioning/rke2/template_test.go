deac558a8992693fe904d8cc3f6f18fb42f204c5
Krunal Hingu <krunal.hingu222@gmail.com>
2025-04-29 09:44:47 +0530

    refactor: remove obsolete deletion tests and related documentation

--- tests/v2/validation/provisioning/rke2/template_test.go
+++ tests/v2/validation/provisioning/rke2/template_test.go
@@ -2,22 +2,13 @@ package rke2
 
 import (
 	"testing"
-	"time"
 
-	"github.com/rancher/rancher/tests/v2/actions/charts"
 	"github.com/rancher/rancher/tests/v2/actions/provisioning"
 	"github.com/rancher/rancher/tests/v2/actions/provisioninginput"
-	"github.com/rancher/rancher/tests/v2/actions/reports"
 	"github.com/rancher/shepherd/clients/rancher"
 	v1 "github.com/rancher/shepherd/clients/rancher/v1"
 	"github.com/rancher/shepherd/extensions/cloudcredentials"
-	"github.com/rancher/shepherd/extensions/clusters"
-	"github.com/rancher/shepherd/extensions/clusters/kubernetesversions"
-	"github.com/rancher/shepherd/extensions/defaults"
-	"github.com/rancher/shepherd/extensions/defaults/stevetypes"
-	"github.com/rancher/shepherd/extensions/steve"
 	"github.com/rancher/shepherd/pkg/config"
-	"github.com/rancher/shepherd/pkg/namegenerator"
 	"github.com/rancher/shepherd/pkg/session"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -60,23 +51,23 @@ func (r *ClusterTemplateTestSuite) SetupSuite() {
 	require.NoError(r.T(), err)
 }
 
-func (r *ClusterTemplateTestSuite) TestProvisionRKE2TemplateCluster() {
-	_, err := steve.CreateAndWaitForResource(r.client, stevetypes.ClusterRepo, r.templateConfig.Repo, true, 5*time.Second, defaults.FiveMinuteTimeout)
-	require.NoError(r.T(), err)
+// func (r *ClusterTemplateTestSuite) TestProvisionRKE2TemplateCluster() {
+// 	_, err := steve.CreateAndWaitForResource(r.client, stevetypes.ClusterRepo, r.templateConfig.Repo, 5*time.Second, defaults.FiveMinuteTimeout)
+// 	require.NoError(r.T(), err)
 
-	k8sversions, err := kubernetesversions.Default(r.client, providerName, nil)
-	require.NoError(r.T(), err)
+// 	k8sversions, err := kubernetesversions.Default(r.client, providerName, nil)
+// 	require.NoError(r.T(), err)
 
-	clusterName := namegenerator.AppendRandomString(providerName + "-template")
-	err = charts.InstallTemplateChart(r.client, r.templateConfig.Repo.ObjectMeta.Name, r.templateConfig.TemplateName, clusterName, k8sversions[0], r.cloudCredentials)
-	require.NoError(r.T(), err)
+// 	clusterName := namegenerator.AppendRandomString(providerName + "-template")
+// 	err = charts.InstallTemplateChart(r.client, r.templateConfig.Repo.ObjectMeta.Name, r.templateConfig.TemplateName, clusterName, k8sversions[0], r.cloudCredentials)
+// 	require.NoError(r.T(), err)
 
-	_, cluster, err := clusters.GetProvisioningClusterByName(r.client, clusterName, fleetNamespace)
-	reports.TimeoutClusterReport(cluster, err)
-	require.NoError(r.T(), err)
+// 	_, cluster, err := clusters.GetProvisioningClusterByName(r.client, clusterName, fleetNamespace)
+// 	reports.TimeoutClusterReport(cluster, err)
+// 	require.NoError(r.T(), err)
 
-	provisioning.VerifyCluster(r.T(), r.client, nil, cluster)
-}
+// 	provisioning.VerifyCluster(r.T(), r.client, nil, cluster)
+// }
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
